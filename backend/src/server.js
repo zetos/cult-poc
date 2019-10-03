15958afa41cc6dfa3e1015e232b3190ea816b429
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-03 19:36:15 -0300

    Setup Atlas MongoDB connection & dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
-const routes = require('./routes')
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const routes = require('./routes');
 
 const app = express();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(err => console.error('Mongoose connect error:', err))
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully..');
+});
 
 app.use(express.json());
 app.use(routes);
