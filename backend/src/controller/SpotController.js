6e28020be6f5b86daf9af50573278613c1edc17b
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-04 00:38:01 -0300

    create Spot on db

--- backend/src/controller/SpotController.js
+++ backend/src/controller/SpotController.js
@@ -1,14 +1,27 @@
 const Spot = require('../models/Spot');
+const User = require('../models/User');
 
 const store = async (req, res) => {
   try {
-    // const { email } = req.body;
-    console.log(req.body);
-    console.log(req.file);
-    return res.json({ message: 'spot success' });
+    const { filename } = req.file;
+    const { cult, deities, price } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      cult,
+      deities: deities.split(',').map(deitie => deitie.trim()),
+      price
+    });
+
+    return user
+      ? res.json(spot)
+      : res.status(400).json({ error: 'User does not exists.' });
   } catch (err) {
-    console.error('SessionController store error:', err);
-    return res.status(500).send(err.errmsg);
+    return res.status(400).send({ error: err.message });
   }
 };
 
