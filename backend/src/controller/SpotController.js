4c79b33e90bbb6bb0462b9975848a6230a8cfc15
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-04 00:48:48 -0300

    Filter by deity route & typo fix

--- backend/src/controller/SpotController.js
+++ backend/src/controller/SpotController.js
@@ -1,6 +1,15 @@
 const Spot = require('../models/Spot');
 const User = require('../models/User');
 
+const index = async (req, res) => {
+  // Filter by deity.
+  const { deity } = req.query;
+
+  const spots = await Spot.find({ deities: deity });
+
+  return res.json(spots);
+};
+
 const store = async (req, res) => {
   try {
     const { filename } = req.file;
@@ -8,23 +17,25 @@ const store = async (req, res) => {
     const { user_id } = req.headers;
 
     const user = await User.findById(user_id);
+    if (!user) {
+      return res.status(400).json({ error: 'User does not exists.' });
+    }
 
     const spot = await Spot.create({
       user: user_id,
       thumbnail: filename,
       cult,
-      deities: deities.split(',').map(deitie => deitie.trim()),
+      deities: deities.split(',').map(deity => deity.trim()),
       price
     });
 
-    return user
-      ? res.json(spot)
-      : res.status(400).json({ error: 'User does not exists.' });
+    return res.json(spot);
   } catch (err) {
     return res.status(400).send({ error: err.message });
   }
 };
 
 module.exports = {
-  store
+  store,
+  index
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,8 @@ const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
 
 module.exports = routes;
