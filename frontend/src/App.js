84195c81ed8b66e673c4565a15b21978c2fe9c81
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-04 23:33:35 -0300

    Record created user id in local state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import api from './services/api';
 import './App.css';
 import logo from './assets/logo.svg'
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', {email});
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
     <img src={logo} alt="Cult-poc"/>
@@ -13,9 +25,15 @@ function App() {
     Ofereça <strong>spots</strong> para cultistas e encontre <strong>talentos</strong> para sua ordem.
     </p>
 
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="email">E-MAIL *</label>
-      <input type="email" id="email" placeholder="Seu e-mail.." />
+      <input 
+      type="email" 
+      id="email" 
+      placeholder="Seu e-mail.." 
+      value={email}
+      onChange={event => setEmail(event.target.value)} 
+      />
 
       <button className="btn" type="submit">Entrar</button>
     </form>
