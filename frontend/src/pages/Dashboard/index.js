09dec94f09d16254717e3cbfe48c5a3d5fd1c2e2
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-05 01:14:05 -0300

    Dashboard page

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+
+import api from '../../services/api';
+import './styles.css';
 
 export default function Dashboard() {
-    return <div />
+    const [spots, setSpots] = useState([]);
+
+    useEffect(() => {
+        // immediately invoked function
+        (async () => {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard', {
+                headers: { user_id }
+            });
+
+            setSpots(response.data);
+          })()
+    }, []);
+
+    return (
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id} >
+                        <header style={{backgroundImage: `url(${spot.thumbnail_url})`}} />
+                        <strong>{spot.cult}</strong>
+                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO"'}</span>
+                    </li>
+                ))}
+            </ul>
+            <Link to="/new">
+                <button className="btn">Cadastrar novo spot</button>
+            </Link>
+        </>
+        )
 }
