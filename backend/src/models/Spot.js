29ceb8772e8d0a1270f818862a34ba50b83b116b
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-06 19:38:46 -0300

    Remove arrow function

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -20,8 +20,8 @@ const SpotSchema = new Schema(
   { timestamps: true, toJSON: { virtuals: true } }
 );
 
-SpotSchema.virtual('thumbnail_url').get(
-  () => `http://192.168.100.12:3001/files/${this.thumbnail}`
-);
+SpotSchema.virtual('thumbnail_url').get(function() {
+  return `http://192.168.100.12:3001/files/${this.thumbnail}`;
+});
 
 module.exports = mongoose.model('Spot', SpotSchema);
