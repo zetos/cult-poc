f36acf7bae7659ad0c9ce63907d6cf4920a80613
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-07 00:12:15 -0300

    Emit approval/rejection alert to mobile client.

--- backend/src/controller/RejectionController.js
+++ backend/src/controller/RejectionController.js
@@ -8,6 +8,12 @@ const store = async (req, res) => {
     booking.approved = false;
     await booking.save();
 
+    const bookingUserSocket = req.connectedUsers[booking.user];
+
+    if (bookingUserSocket) {
+      req.io.to(bookingUserSocket).emit('booking_response', booking);
+    }
+
     return res.json(booking);
   } catch (err) {
     console.error('RejectionController store error:', err);
